2776ee2350197b568b2b433a12c356a807aa0687
joneisk <joaomarcelogretter2004@gmail.com>
2024-05-17 19:30:54 -0300

    Commit nas services e entidades

--- Projeto_SocialCare/backend/SocialCare/src/main/java/br/unoeste/fipp/socialcare/DataBase/entities/LegalPerson.java
+++ Projeto_SocialCare/backend/SocialCare/src/main/java/br/unoeste/fipp/socialcare/DataBase/entities/LegalPerson.java
@@ -18,13 +18,127 @@ public class LegalPerson extends Person{
     private String site;
 
     public LegalPerson() {
-        this(null,"","","");
+        this(0L,"","","","","",0,null,null,"","","");
     }
 
-    public LegalPerson(Gender genero, String cnpj, String ie, String site) {
+    public LegalPerson(Long id, String nome, String logradouro, String bairro, String cep, String email, int ativo,
+                       City city, Gender genero, String cnpj, String ie, String site) {
+        super(id, nome, logradouro, bairro, cep, email, ativo, city);
         this.genero = genero;
         this.cnpj = cnpj;
         this.ie = ie;
         this.site = site;
+    }
+
+    public Gender getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Gender genero) {
+        this.genero = genero;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getIe() {
+        return ie;
+    }
+
+    public void setIe(String ie) {
+        this.ie = ie;
+    }
+
+    public String getSite() {
+        return site;
+    }
+
+    public void setSite(String site) {
+        this.site = site;
+    }
+
+    @Override
+    public String getNome() {
+        return super.getNome();
+    }
+
+    @Override
+    public void setNome(String nome) {
+        super.setNome(nome);
+    }
+
+    @Override
+    public Long getId() {
+        return super.getId();
+    }
+
+    @Override
+    public void setId(Long id) {
+        super.setId(id);
+    }
+
+    @Override
+    public String getLogradouro() {
+        return super.getLogradouro();
+    }
+
+    @Override
+    public void setLogradouro(String logradouro) {
+        super.setLogradouro(logradouro);
+    }
+
+    @Override
+    public String getBairro() {
+        return super.getBairro();
+    }
+
+    @Override
+    public void setBairro(String bairro) {
+        super.setBairro(bairro);
+    }
+
+    @Override
+    public String getCep() {
+        return super.getCep();
+    }
+
+    @Override
+    public void setCep(String cep) {
+        super.setCep(cep);
+    }
+
+    @Override
+    public String getEmail() {
+        return super.getEmail();
+    }
+
+    @Override
+    public void setEmail(String email) {
+        super.setEmail(email);
+    }
+
+    @Override
+    public int getAtivo() {
+        return super.getAtivo();
+    }
+
+    @Override
+    public void setAtivo(int ativo) {
+        super.setAtivo(ativo);
+    }
+
+    @Override
+    public City getCity() {
+        return super.getCity();
+    }
+
+    @Override
+    public void setCity(City city) {
+        super.setCity(city);
     }
 }
