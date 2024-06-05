f46f9e2bdb4c24573be8840139972501b0bbc305
João Marcelo <joaomarcelogretter2004@gmail.com>
2024-06-05 16:01:19 -0300

    Começo do encerrar acolhimento

--- Projeto_SocialCare/backend/SocialCare/src/main/java/br/unoeste/fipp/socialcare/DataBase/repositories/planRepository.java
+++ Projeto_SocialCare/backend/SocialCare/src/main/java/br/unoeste/fipp/socialcare/DataBase/repositories/planRepository.java
@@ -1,6 +1,5 @@
 package br.unoeste.fipp.socialcare.DataBase.repositories;
 
-import br.unoeste.fipp.socialcare.DataBase.entities.Plan;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface planRepository extends JpaRepository<Plan, Long> {
